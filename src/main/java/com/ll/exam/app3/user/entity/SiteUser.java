9a0de52c44d4bc478f325dda2fac0ee62dcc108c
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-26 13:49:46 +0900

    UserRepository 추가, 회원 추가 테스트 케이스

--- src/main/java/com/ll/exam/app3/user/entity/SiteUser.java
+++ src/main/java/com/ll/exam/app3/user/entity/SiteUser.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app3.user.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import javax.persistence.*;
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
 public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +22,8 @@ public class SiteUser {
 
     @Column(unique = true)
     private String email;
+
+    public SiteUser() {
+
+    }
 }
