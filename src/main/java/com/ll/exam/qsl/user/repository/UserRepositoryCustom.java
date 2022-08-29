5504230a2c9a7731ea076a2041b0630ad9a33ce8
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-29 15:19:13 +0900

    전체회원조회, 가장오래된 순

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryCustom.java
@@ -2,10 +2,14 @@ package com.ll.exam.qsl.user.repository;
 
 import com.ll.exam.qsl.user.entity.SiteUser;
 
+import java.util.List;
+
 public interface UserRepositoryCustom {
     SiteUser getQslUser(Long id);
 
     int getQslCount();
 
     SiteUser getQslUserOrderByIdAscOne();
+
+    List<SiteUser> getQslUsersOrderByIdAsc();
 }
